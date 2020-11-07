cb3854172ed8bda10bd35fe70aca049af44cb38a
Santiago Rubiano <43104565+srubianof@users.noreply.github.com>
2020-11-07 00:24:24 -0500

    Update sonarqube-remove-branch-name.ps1

--- sonarqube-remove-branch-name.ps1
+++ sonarqube-remove-branch-name.ps1
@@ -1,3 +1,2 @@
-- powershell: |
-    $params = "$env:SONARQUBE_SCANNER_PARAMS" -replace '"sonar.branch.name":"[\w,/,-]*"\,?'
-    Write-Host "##vso[task.setvariable variable=SONARQUBE_SCANNER_PARAMS]$params"
+$params = "$env:SONARQUBE_SCANNER_PARAMS" -replace '"sonar.branch.name":"[\w,/,-]*"\,?'
+Write-Host "##vso[task.setvariable variable=SONARQUBE_SCANNER_PARAMS]$params"
