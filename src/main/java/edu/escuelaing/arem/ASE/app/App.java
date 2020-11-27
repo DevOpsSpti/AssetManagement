4fcf6fadd0e3bd6b836cbd9526551e1d8133be3f
AlejandroBohal <sergio.bohorquez@mail.escuelaing.edu.co>
2020-11-27 02:04:40 -0500

    Fixed bugs

--- src/main/java/edu/escuelaing/arem/ASE/app/App.java
+++ src/main/java/edu/escuelaing/arem/ASE/app/App.java
@@ -1,12 +1,5 @@
 package edu.escuelaing.arem.ASE.app;
 
-import org.eclipse.jetty.util.log.Log;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
-
 public class App{
 	/**
 	 * @author Jose Luis Gomez Camacho
@@ -17,7 +10,7 @@ public class App{
 			System.out.println(calculos.mean());
 			System.out.println(calculos.stddev());
 		} catch (ListaException e) {
-			Logger.getAnonymousLogger().log(Level.SEVERE, "an exception was thrown", e);
+			e.printStackTrace();
 		}
 	}
 }
